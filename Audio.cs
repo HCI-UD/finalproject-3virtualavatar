330bdc898085dfbed3b54f2017f0df27ca8e41ce
WeitongSun <47732803+WeitongSun@users.noreply.github.com>
2021-12-17 13:53:14 -0500

    Add files via upload
    Added Audio script

---
+++ Audio.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    public AudioSource mySound;
+    public AudioClip clickSound;
+
+    public void ClickSound(){
+        mySound.PlayOneShot(clickSound);
+    }
+}
